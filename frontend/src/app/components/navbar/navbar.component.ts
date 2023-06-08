a41351384d159019f8156eba689bf746efac8ac6
Kylian Bardini <kylianb1@icloud.com>
2023-06-08 13:25:32 +0200

    💄 update: add ui logic when user isLogged

--- frontend/src/app/components/navbar/navbar.component.ts
+++ frontend/src/app/components/navbar/navbar.component.ts
@@ -1,10 +1,38 @@
 import { Component } from '@angular/core';
+import { AuthentificationService } from 'src/app/services/authentification.service';
+import Jwt from 'src/app/models/jwt';
+import User from 'src/app/models/user';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent {
+  jwt: Jwt | null = null;
+  user: User | null = this.getUser();
 
+  constructor(private auth: AuthentificationService) {
+    this.auth.$jwt.subscribe({
+      next: (jwt) => {
+        this.jwt = jwt;
+      },
+      error: (error) => {
+        console.log(error);
+      },
+    });
+  }
+
+  getUser() {
+    const user = localStorage.getItem('user');
+    if (user) {
+      return JSON.parse(user);
+    } else {
+      return null;
+    }
+  }
+
+  signOut() {
+    this.auth.logout();
+  }
 }
